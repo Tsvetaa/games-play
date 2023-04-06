13413721e715a82b21befd037bbd4e1f6589be9c
petkots <tsveta.v.petkova@gmail.com>
2023-04-06 21:45:48 +0200

    Router Workshop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import * as gameService from './services/gameService';
+
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Home } from "./components/Home/Home";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { CreateGame } from "./components/CreateGame/CreateGame";
+import { Catalog } from "./components/Catalog/Catalog";
+import { GameDetails } from "./components/GameDetails/GameDetails";
 
 function App() {
+    const navigate = useNavigate();
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+      gameService.getAll()
+          .then(result => {
+            setGames(result)
+          })
+    }, []);
+
+    const onCreateGameSubmit = async (data) => {
+      const newGame = await gameService.create(data);
+
+      setGames(state => [...state, newGame]);
+
+      navigate('/catalog');
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="box">
+      <Header />
+
+      <main id="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/create-game" element={<CreateGame onCreateGameSubmit={onCreateGameSubmit}/>} />
+          <Route path="/catalog" element={<Catalog games={games} />} />
+          <Route path='/catalog/:gameId' element={<GameDetails/>} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
